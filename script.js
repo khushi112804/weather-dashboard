d696f93b8c177e65e52f9728a33bb335c2905ad1
Khushi Jain <kjain282004@gmail.com>
2026-02-25 23:49:20 +0530

    Added interactive map card using coordinates

--- script.js
+++ script.js
@@ -1,18 +1,56 @@
+
+function toggleTheme(){
+  document.body.classList.toggle("dark");
+}
+
 async function searchCity(){
 
-const city = document.getElementById("city").value;
+const city=document.getElementById("city").value;
 const apiKey="aaf5a944983235268ed1569d5de2bed2";
 
 const url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
 const res=await fetch(url);
 const data=await res.json();
-document.getElementById("cityTitle").innerText=data.name;
+
 if(data.cod==200){
+
+document.getElementById("cityTitle").innerText=data.name;
+
 document.getElementById("weather").innerText=
-data.main.temp + "°C • " + data.weather[0].main;
-}
-function toggleTheme(){
-document.body.classList.toggle("dark");
+data.main.temp+"°C • "+data.weather[0].main;
+
+// AQI
+const lat=data.coord.lat;
+const lon=data.coord.lon;
+
+const aqiUrl=
+`https://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+
+const aqiRes=await fetch(aqiUrl);
+const aqiData=await aqiRes.json();
+
+const aqi=aqiData.list[0].main.aqi;
+
+document.getElementById("aqiValue").innerText="AQI: "+aqi;
+
+let status="";
+if(aqi==1) status="Good";
+else if(aqi==2) status="Fair";
+else if(aqi==3) status="Moderate";
+else if(aqi==4) status="Poor";
+else status="Very Poor";
+
+document.getElementById("aqiStatus").innerText=status;
+
+// color feedback
+const card=document.getElementById("aqiCard");
+
+if(aqi<=2) card.style.background="#d1fae5";
+else if(aqi==3) card.style.background="#fef3c7";
+else card.style.background="#fee2e2";
+document.getElementById("map").src =
+`https://maps.google.com/maps?q=${lat},${lon}&z=12&output=embed`;
+
 }
 }
