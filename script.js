5ce02413baec8f613e73b8f3ea8de7deb2648606
Khushi Jain <kjain282004@gmail.com>
2026-02-25 23:59:44 +0530

    Added favorites feature with persistent localStorage

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-
+// Dark mode
 function toggleTheme(){
   document.body.classList.toggle("dark");
 }
 
+// MAIN SEARCH
 async function searchCity(){
 
 const city=document.getElementById("city").value;
@@ -17,6 +18,7 @@ if(data.cod==200){
 
 document.getElementById("cityTitle").innerText=data.name;
 
+// WEATHER
 document.getElementById("weather").innerText=
 data.main.temp+"°C • "+data.weather[0].main;
 
@@ -43,14 +45,54 @@ else status="Very Poor";
 
 document.getElementById("aqiStatus").innerText=status;
 
-// color feedback
+// AQI color
 const card=document.getElementById("aqiCard");
 
 if(aqi<=2) card.style.background="#d1fae5";
 else if(aqi==3) card.style.background="#fef3c7";
 else card.style.background="#fee2e2";
+
+// MAP
 document.getElementById("map").src =
 `https://maps.google.com/maps?q=${lat},${lon}&z=12&output=embed`;
 
 }
 }
+
+// SAVE FAVORITES
+function saveCity(){
+
+const city=document.getElementById("cityTitle").innerText;
+
+let fav=JSON.parse(localStorage.getItem("favCities"))||[];
+
+if(!fav.includes(city)){
+fav.push(city);
+localStorage.setItem("favCities",JSON.stringify(fav));
+renderFavorites();
+}
+}
+
+// RENDER FAVORITES
+function renderFavorites(){
+
+const fav=JSON.parse(localStorage.getItem("favCities"))||[];
+const container=document.getElementById("favorites");
+
+container.innerHTML="";
+
+fav.forEach(city=>{
+const btn=document.createElement("button");
+btn.innerText=city;
+
+btn.onclick=()=>{
+document.getElementById("city").value=city;
+searchCity();
+};
+
+container.appendChild(btn);
+});
+}
+
+// LOAD favorites on start
+renderFavorites();
